1a2a95ad99182d5016028e6af1c24d96d00154d7
Virginia Wanjiru <virginiashee55@gmail.com>
2024-01-29 21:52:27 +0300

    refactor: Add Programming Languages

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,13 +1,7 @@
 // Skills.js
 import React from 'react';
-import Python from '../images/python.svg';
-import R from '../images/R.svg';
-import CPlusPlus from '../images/CPlusPlus.svg';
-import Csharp from '../images/csharp.svg';
-import Java from '../images/java.svg';
-import JS from '../images/JS.svg';
-import Kotlin from '../images/kotlin.svg';
-import Visual from '../images/visual-studio-code.svg';
+
+
 
 function Skills() {
   return (
@@ -15,14 +9,15 @@ function Skills() {
       <h2>Programming Languages and IDES </h2>
       <ul className="creative-skills-list">
         
-      <li><img src={Python} alt="Python Logo" style={{ width: '100px', height: '100px' }} /></li>
-      <li><img src={R} alt="R Logo" style={{ width: '100px', height: '100px' }} /></li>
-      <li><img src={Csharp} alt="C Logo" style={{ width: '100px', height: '100px' }} /></li>
-      <li><img src={CPlusPlus} alt="C# Logo" style={{ width: '100px', height: '100px' }} /></li>
-      <li><img src={Java} alt="Java Logo" style={{ width: '100px', height: '100px' }} /></li>
-      <li><img src={JS} alt="JavaScript Logo" style={{ width: '100px', height: '100px' }} /></li>
-      <li><img src={Kotlin} alt="Kotlin Logo" style={{ width: '100px', height: '75px' }} /></li>
-      <li><img src={Visual} alt="VS Logo" style={{ width: '100px', height: '100px' }} /></li>
+      <li>PYTHON</li>
+      <li>R</li>
+      <li>C#</li>
+      <li>C++</li>
+      <li>PHP</li>
+      <li>VISUAL STUDIO </li>
+      <li></li>
+      <li>JAVA </li>
+      <li>JAVASCRIPT</li>
       
         
       </ul>
